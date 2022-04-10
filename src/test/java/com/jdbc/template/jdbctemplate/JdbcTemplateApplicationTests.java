ce83dd28ff5631233ec3e46e44a211c547185bdb
aymenbraiek <aymen.braiek@esprit.tn>
2022-04-10 14:42:25 +0100

    Initial project

---
+++ src/test/java/com/jdbc/template/jdbctemplate/JdbcTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jdbc.template.jdbctemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
